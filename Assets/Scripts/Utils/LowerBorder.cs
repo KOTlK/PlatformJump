f0b0f8359aaaf42605807cabbc06108611770046
KOTlK <rojuty@gmail.com>
2022-02-18 14:06:22 +0700

    fix

--- Assets/Scripts/Utils/LowerBorder.cs
+++ Assets/Scripts/Utils/LowerBorder.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class LowerBorder : MonoBehaviour
 {
     public event Action<Platform> BorderTouched;
+    private void Awake()
+    {
+        var collider = GetComponent<BoxCollider2D>();
+        collider.isTrigger = true;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
